9fb96c421a1af332fc31ffef29a85703e8ac6cf9
Petar Stanev <petarstanev@outlook.com>
2017-12-03 21:50:35 +0000

    Changed folder structure for css.

--- ProjectManagmentApplication/App_Start/BundleConfig.cs
+++ ProjectManagmentApplication/App_Start/BundleConfig.cs
@@ -24,8 +24,8 @@ namespace ProjectManagmentApplication
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/css/bootstrap.css",
+                      "~/Content/css/mainstyle.css"));
         }
     }
 }
