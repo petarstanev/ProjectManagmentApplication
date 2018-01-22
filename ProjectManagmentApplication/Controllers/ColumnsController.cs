da3a238aefd96260f13f1b4721e643da415199ae
Petar Stanev <petarstanev@outlook.com>
2018-01-22 16:36:15 +0000

    Columns now are working.

--- ProjectManagmentApplication/Controllers/ColumnsController.cs
+++ ProjectManagmentApplication/Controllers/ColumnsController.cs
@@ -1,125 +1,137 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using ProjectManagmentApplication.Models;
-using ProjectManagmentApplication.ViewModels;
 
 namespace ProjectManagmentApplication.Controllers
 {
-    public class BoardsController : Controller
+    public class ColumnsController : Controller
     {
         private Context db = new Context();
 
-        // GET: Boards
+        // GET: Columns
         public ActionResult Index()
         {
-            return View(db.Boards.ToList());
+            return View(db.Columns.ToList());
         }
 
-        // GET: Boards/Details/5
+        // GET: Columns/Details/5
         public ActionResult Details(int? id)
         {
-            BoardViewModel boardView = new BoardViewModel();
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Board board = db.Boards.Include(e => e.Columns).SingleOrDefault(e => e.BoardId == id);
-            if (board == null)
+            Column column = db.Columns.Find(id);
+            if (column == null)
             {
                 return HttpNotFound();
             }
-            boardView.SelectedBoard = board;
-            boardView.AllBoards = db.Boards.ToList();
-
-            return View(boardView);
+            return View(column);
         }
 
-        // GET: Boards/Create
-        public ActionResult Create()
+        // GET: Columns/Create
+        public ActionResult Create(int boardId)
         {
-            return View();
+            if (boardId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //Board board = db.Boards.Include(e => e.Columns).SingleOrDefault(e => e.BoardId == id);
+            Board board = db.Boards.SingleOrDefault(e => e.BoardId == boardId);
+
+            Column column = new Column();
+
+            column.Board = board;
+
+            return View(column);
         }
 
-        // POST: Boards/Create
+
+        // POST: Columns/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "BoardId,Title")] Board board)
+        public ActionResult Create(Column column)
         {
             if (ModelState.IsValid)
             {
-                db.Boards.Add(board);
+                Board board = db.Boards.Include(p => p.Columns).SingleOrDefault(p => p.BoardId == column.Board.BoardId);
+                column.Board = board;
+                db.Columns.Add(column);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("Index","Boards");
             }
 
-            return View(board);
+            return View(column);
         }
 
-        // GET: Boards/Edit/5
+        // GET: Columns/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Board board = db.Boards.Find(id);
-            if (board == null)
+            Column column = db.Columns.Find(id);
+            if (column == null)
             {
                 return HttpNotFound();
             }
-            return View(board);
+            return View(column);
         }
 
-        // POST: Boards/Edit/5
+        // POST: Columns/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "BoardId,Title")] Board board)
+        public ActionResult Edit([Bind(Include = "ColumnId,Title")] Column column)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(board).State = EntityState.Modified;
+                db.Entry(column).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(board);
+            return View(column);
         }
 
-        // GET: Boards/Delete/5
+        // GET: Columns/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Board board = db.Boards.Find(id);
-            if (board == null)
+            Column column = db.Columns.Find(id);
+            if (column == null)
             {
                 return HttpNotFound();
             }
-            return View(board);
+            return View(column);
         }
 
-        // POST: Boards/Delete/5
+        // POST: Columns/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Board board = db.Boards.Include(p => p.Columns).SingleOrDefault(p => p.BoardId == id);
-
-            db.Columns.RemoveRange(board.Columns);
-            db.Boards.Remove(board);
+            Column column = db.Columns.Find(id);
+            db.Columns.Remove(column);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
