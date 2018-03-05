3044a17bfebe62c0a2181558b29bcce714514a97
Petar Stanev <petarstanev@outlook.com>
2018-03-05 15:55:45 +0000

    Moved js to file.

--- ProjectManagmentApplication/Controllers/TasksController.cs
+++ ProjectManagmentApplication/Controllers/TasksController.cs
@@ -111,7 +111,7 @@ namespace ProjectManagementApplication.Controllers
             {
                 db.Entry(task).State = EntityState.Modified;
                 db.SaveChanges();
-                var hubContext = GlobalHost.ConnectionManager.GetHubContext<BoardHub>();
+                var hubContext = GlobalHost.ConnectionManager.GetHubContext<TaskHub>();
                 hubContext.Clients.All.taskUpdated(task.TaskId);
 
                 return  RedirectToAction("Details", new { id = task.TaskId });
---
+++ ProjectManagmentApplication/Scripts/myscripts/taskupdate.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    var taskId = $("#taskid").val();
+    var hub = $.connection.taskHub;
+
+    hub.client.taskUpdated = function (updatedTaskId) {
+        //console.log("refresh" + updatedTaskId);
+        if (taskId == updatedTaskId) {
+            updateDetails();
+        }
+    };
+
+    $.connection.hub.start();
+
+    function updateDetails() {
+        //console.log(taskId);
+        $.ajax({
+            url: "/Tasks/DetailsGet",
+            type: "GET",
+            data: {
+                id: taskId
+            }
+        }).done(function (partialViewResult) {
+            $("#task-details-container").html(partialViewResult);
+        });
+    }
+    updateDetails();
+});
